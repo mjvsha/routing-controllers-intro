62ab6dd43b792a0839325fa5a28b30e812bcc258
Meera <mjvsharma@outlook.com>
2017-10-11 12:47:09 -0400

    added different headings to the different pages by using instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,28 @@
 class PagesController < ApplicationController
-end
+
 
 #this controller has the sae name as the app/views/pages subdirectory
 #rails by default will look for the erb files that match its own name in the app subdirectory
-#therefore naming is important 
+#therefore naming is important
+
+
+def welcome
+#with nothing inside, this method will automatically render the page associated with is name
+#BUT, i can override this with the render method
+
+@header = "WELCOME!!!!!!"
+end
+
+
+def about
+@header = "ABOUT, SON!!!!!!!!!!!"
+
+end
+
+
+def contest
+@header = "WIN SHIT!!!!!!!!!!!"
+
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/welcome' => 'pages#welcome'
+  #this will run the welcome instance inside the pages controller
+  # the hash rocket defines the route
+
+
+  get '/about' => 'pages#about'
+
+  get '/contest' => 'pages#contest'
+
 end
