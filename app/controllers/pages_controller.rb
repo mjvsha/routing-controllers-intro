88aa119da2c437b585a35325d617394bb9997045
Meera <mjvsharma@outlook.com>
2017-10-11 12:24:40 -0400

    added pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+end
+
+#this controller has the sae name as the app/views/pages subdirectory
+#rails by default will look for the erb files that match its own name in the app subdirectory
+#therefore naming is important 
